6627d83c67f3582649d968ab3d288d509cb8ee30
UBAH <gorodkow2002@gmail.com>
2020-11-13 20:22:26 +0300

    Float and SUM of floats

---
+++ Main.java
@@ -0,0 +1,12 @@
+package suda;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyFloat a = new MyFloat(12343445, 6);
+        MyFloat b = new MyFloat(8656645, 4);
+
+        a.sum(b);
+    }
+}
